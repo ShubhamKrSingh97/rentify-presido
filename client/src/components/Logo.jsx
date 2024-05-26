133a42ac694436f3839036a1ea244f98c9268146
indranil316 <indranil.karmakar@aot.edu.in>
2024-05-26 16:26:17 +0530

    added logo

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo'>Rentify</div>
+  )
+}
+
+export default Logo
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,80 @@
+// src/components/Login.js
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { saveToken, saveUserId, setIsAuthenticated, saveRole } from "../session";
+import Logo from '../components/Logo';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/auth/login', formData)
+    .then(res=>{
+      saveToken(res.data.token);
+      setIsAuthenticated(true);
+      saveUserId(res.data.userId);
+      saveRole(res.data.role)
+      navigate('/')
+    })
+    .catch(err=>{
+      console.log(err)
+      alert(err.response.data)
+      setFormData({
+        email: '',
+        password: '',
+      })
+    })
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className='flex-row-center'>
+        <Logo/>
+        <h1 className="text-center mb-4 header">Login</h1>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100" >
+          Login
+        </button>
+      </form>
+      <div className="text-center mt-3">
+        <p className="mb-0">Don't have an account?</p>
+        <Link to="/register" className="btn btn-link">Register here</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
